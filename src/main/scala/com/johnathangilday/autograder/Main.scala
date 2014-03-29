1e000f58f250a039a864129b232c777ffda72c53
Johnathan Gilday <me@johnathangilday.com>
2014-03-29 00:36:09 -0400

    #5 use heroku assigned port

--- src/main/scala/com/johnathangilday/autograder/Main.scala
+++ src/main/scala/com/johnathangilday/autograder/Main.scala
@@ -6,6 +6,10 @@ import spray.can.Http
 
 object Main extends App {
 
+  val host = "0.0.0.0"
+  val port = Option(System.getenv("PORT")).getOrElse("8080").toInt
+  println("binding to port " + port)
+
   // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("auto-grader")
 
@@ -13,5 +17,5 @@ object Main extends App {
   val service = system.actorOf(Props[HelloHttpActor], "ping-service")
 
   // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ! Http.Bind(service, "localhost", port = 8000)
+  IO(Http) ! Http.Bind(service, host, port)
 }
