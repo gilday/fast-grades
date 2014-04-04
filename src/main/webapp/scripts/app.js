043afb878894e4468c3296c7779590f54a8a1e0f
Johnathan Gilday <me@johnathangilday.com>
2014-04-03 23:11:39 -0400

    #10 Enter the number of answers then fill in each answer with one choice: a,b,c,d

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -9,3 +9,26 @@ var fastgrades = angular.module('fastgrades', []);
 fastgrades.controller('TestCtrl', ['$scope', function ($scope) {
 	$scope.greeting = 'it works!';
 }]);
+
+fastgrades.controller('ExamCtrl', ['$scope', '$location', '$anchorScroll', function ($scope, $location, $anchorScroll) {
+
+    $scope.answerKey = null;
+
+    $scope.isStepOneComplete = function() {
+        return $scope.answerKey !== null;
+    };
+
+    $scope.createAnswerKey = function (numQuestions) {
+        console.log('creating ' + numQuestions + ' questions');
+        $scope.answerKey = new Array(numQuestions);
+        for (var i = 0; i < $scope.answerKey.length; i++) {
+            $scope.answerKey[i] = { value: null };
+        }
+        scrollToStepTwo();
+    };
+
+    function scrollToStepTwo () {
+        $location.hash('step-two');
+        $anchorScroll();
+    }
+}]);
