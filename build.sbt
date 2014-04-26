d0c00a43025eae5c4da4f5cf803389d925090128
Johnathan Gilday <me@johnathangilday.com>
2014-04-25 23:16:09 -0400

    #12 server accepts and grades uploaded exams

--- build.sbt
+++ build.sbt
@@ -19,6 +19,8 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.0" % "test",
   "com.typesafe" %% "scalalogging-slf4j" % "1.0.1",
   "org.scalatra" %% "scalatra" % "2.2.2",
+  "org.scalatra" %% "scalatra-json" % "2.2.2",
+  "org.json4s"   %% "json4s-jackson" % "3.2.6",
   "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106",
   "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "provided"
 )
--- src/main/scala/com/johnathangilday/autograder/FastGradesServlet.scala
+++ src/main/scala/com/johnathangilday/autograder/FastGradesServlet.scala
@@ -2,10 +2,16 @@ package com.johnathangilday.autograder
 
 import org.scalatra.{BadRequest, ScalatraServlet}
 import org.scalatra.servlet.{FileItem, FileUploadSupport}
+import org.json4s.{DefaultFormats, Formats}
 import java.io.File
+import org.scalatra.json.JacksonJsonSupport
 
-class FastGradesServlet extends ScalatraServlet with FileUploadSupport {
+class FastGradesServlet extends ScalatraServlet with FileUploadSupport with JacksonJsonSupport {
   this: Core =>
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  val json = formats("json")
 
   get("/") {
     "It works!"
@@ -13,14 +19,15 @@ class FastGradesServlet extends ScalatraServlet with FileUploadSupport {
 
   post("/files") {
     fileParams.get("exam") match {
-      case Some(file) => file.copy()
+      case Some(fileItem) => gradeExamFile(fileItem)
       case None => BadRequest("Missing file")
     }
   }
 
   def gradeExamFile(fileItem: FileItem) = {
+    contentType = json
     val file = save(fileItem)
-    // TODO grade the file
+    grader.grade(file)
   }
 
   def save(fileItem: FileItem): File = {
